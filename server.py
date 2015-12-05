d59146931fbf1f3162683bcfe07d7299a9d61732
TinnoTL <wethealienz@gmail.com>
2015-12-05 16:45:36 +0100

    server.py modified

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path='/static')
 
-index_html = open("index.html").read()
+
 
 @app.route("/")
-def hello():
-    return index_html
+def home():
+    return render_template("index.html", title = 'Index')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run('localhost', 5001)
