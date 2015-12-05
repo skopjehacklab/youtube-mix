860f2dbf5d1b92c7bfd1ca6bdb444c9ba69d569f
Andrej T <andrejtrajchevski@gmail.com>
2015-12-05 19:44:48 +0100

    you don't wanna know

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory, g
 import os
 import subprocess
+import glob
 app = Flask(__name__, static_url_path='/static')
 app.debug = True
 
@@ -16,24 +17,36 @@ def page_not_found(error):
 def generate():
 	playlist = request.form["playlist"]
 
-	
-
 	try:
-		os.makedirs('./Music/')
+		os.makedirs('./music/')
 	except OSError:
 		pass
+		
+	os.chdir('./music')
 
-	playlist_txt = open("Music/playlist.txt",'w')
+	playlist_txt = open("playlist.txt",'w')
 	playlist_txt.write(playlist)
 	playlist_txt.close()
 
-	subprocess.call(["youtube-dl", "--output", "Music/%(autonumber)s.%(ext)s", "--extract-audio", "--audio-format", "mp3", "--audio-quality", "9", "--add-metadata", "--no-overwrites", "--continue", "--batch-file", "Music/playlist.txt"])
+	subprocess.call(["youtube-dl", "--output", "%(autonumber)s.%(ext)s",
+		"--extract-audio", "--audio-format", "aac", "--audio-quality", "0",
+		"--add-metadata", "--no-overwrites", "--continue", "--batch-file",
+		"playlist.txt"])
 
-	songs_txt = open("Music/songs.txt",'a')
-	songs_txt.close()
+	is_a_song = lambda p: '.aac' in p and p != 'out.aac'
+	songs_filenames = filter(is_a_song, sorted(os.listdir('./')))
 	
+	ffmpeg_concat = 'concat:' + '|'.join(songs_filenames) # 01.aac|02.aac
+	
+	subprocess.call(["ffmpeg", "-i", ffmpeg_concat, '-acodec', 'copy', '-y', 'out.aac'])
+	
+	for f in glob.glob('0*.aac'): # uzhas (tomi)! :D
+		os.remove(f)
+	os.remove('playlist.txt')
+	
+	os.chdir('..')
 
-	return "Hello from generate"
+	return send_from_directory('./music', 'out.aac', as_attachment=True)
 
 if __name__ == "__main__":
     app.run('localhost', 5001)
