5278fb4ab64a50098a38f9c7f12389bb7ec0d211
TinnoTL <wethealienz@gmail.com>
2015-12-05 17:57:30 +0100

    server.py adds music folder with .txt files

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 app = Flask(__name__, static_url_path='/static')
 app.debug = True
 
@@ -13,7 +14,23 @@ def page_not_found(error):
 @app.route("/generate", methods=["POST"])
 def generate():
 	playlist = request.form["playlist"]
-	print playlist
+
+	
+
+	try:
+		os.makedirs('./Music/')
+	except OSError:
+		pass
+
+	playlist_txt = open("Music/playlist.txt",'w')
+	playlist_txt.write(playlist)
+	playlist_txt.close()
+
+
+	songs_txt = open("Music/songs.txt",'a')
+	songs_txt.close()
+	
+
 	return "Hello from generate"
 
 if __name__ == "__main__":
