2e8144ca1a7a1bd325faff6168fe04b00ca65d24
newkdukem <newkdukem@gmail.com>
2015-12-05 16:48:00 +0100

    added generate in server.py

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
+app.debug = True
 
 index_html = open("index.html").read()
 
 @app.route("/")
-def hello():
+def index():
     return index_html
+
+@app.route("/generate", methods=["POST"])
+def generate():
+	return "Hello from generate"
 
 if __name__ == "__main__":
     app.run()
